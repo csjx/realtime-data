365e13e273b4a391f62d56b248b4e16fa0d94e1f
Christopher Jones <csj@christopherjones.co>
2007-08-05 16:30:48 +0000

    I'm adding in all of the fields for the FixedLeader as ByteBuffers that will be populated from the ensemble byte stream.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@68 9e959af2-1677-4b95-8350-622619c7fe3b

--- src/java/edu/hawaii/soest/kilonalu/adcp/EnsembleFixedLeader.java
+++ src/java/edu/hawaii/soest/kilonalu/adcp/EnsembleFixedLeader.java
@@ -41,6 +41,228 @@ import java.nio.ByteOrder;
 public final class EnsembleFixedLeader {
 
   /**
+   *  A field that contains the base frequency index for Navigator only,
+   *  and is considered a spare for the Workhorse.
+   */
+  private ByteBuffer baseFrequencyIndex         = ByteBuffer.allocate(1);
+
+  /**
+   *  A field that contains the beam angle (H-ADCP only). This byte is 
+   *  considered a spare for all other ADCPs.
+   */
+  private ByteBuffer beamAngle                  = ByteBuffer.allocate(1);
+
+  /**
+   *  A field that contains the distance to the middle of the first depth
+   *  cell (bin).  This distance is a function of depth cell length, 
+   *  the profiling mode, the blank after transmit distance.
+   */
+  private ByteBuffer binOneDistance             = ByteBuffer.allocate(2);
+  
+  /**
+   *  A field that contains the blanking distance used by the Workhorse 
+   * to allow the transmit circuits time to recover before the receive 
+   * cycle begins.
+   */
+  private ByteBuffer blankAfterTransmit         = ByteBuffer.allocate(2);
+  
+  /**
+   *  A field that contains the coordinate transformation processing 
+   *  parameters.  These firmware switches indicate how the Workhorse 
+   *  collected data.  
+   *  See the manual for interpreting each bit. 
+   */
+  private ByteBuffer coordinateTransformParams  = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that contains the revision number of the CPU firmware.
+   */
+  private ByteBuffer cpuFirmwareRevision        = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that contains the version number of the CPU firmware.
+   */
+  private ByteBuffer cpuFirmwareVersion         = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that contains the serial number of the CPU board.
+   */
+  private ByteBuffer cpuBoardSerialNumber       = ByteBuffer.allocate(8);
+  
+  /**
+   *  A field that contains the length of one depth cell.
+   */
+  private ByteBuffer depthCellLength            = ByteBuffer.allocate(2);
+  
+  /**
+   * A field that contains the actual threshold value used to flag 
+   * water-current data as good or bad.  If the error velocity value 
+   * exceeds this threshold, the Workhorse flags all four beams of the 
+   * affected bin as bad.
+   */
+  private ByteBuffer errorVelocityThreshold     = ByteBuffer.allocate(2);
+  
+  /**
+   *  A field that contains the threshold value used to reject data 
+   * received from a false target, usually fish.
+   */
+  private ByteBuffer falseTargetThreshold       = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that stores the default Fixed Leader ID (0x0000)
+   */
+  private static final ByteBuffer DEFAULT_FIXED_LEADER_ID = 
+    ByteBuffer.wrap( new byte[]{0x00,0x00} );
+
+  /**
+   *  A field that stores the Fixed Leader ID (2-bytes) in a ByteBuffer
+   */
+  private ByteBuffer fixedLeaderID              = ByteBuffer.allocate(2);
+
+  /**
+   *  A field that stores the Fixed Leader Spare (1-byte) in a ByteBuffer
+   *  This field is reserved for RDI internal use.
+   */
+  private ByteBuffer fixedLeaderSpare           = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that contains a correction factor for physical 
+   *  heading misalignment.
+   */
+  private ByteBuffer headingAlignment           = ByteBuffer.allocate(2);
+  
+  /**
+   *  A field that contains a correction factor for electrical/magnetic 
+   *  heading bias.  
+   */
+  private ByteBuffer headingBias                = ByteBuffer.allocate(2);
+  
+    /**
+     * A field stores the lag length, which is the time period between 
+     * sound pulses.
+     */
+  private ByteBuffer lagLength                  = ByteBuffer.allocate(1);
+  
+  /**
+   * A field stores Contains the minimum threshold of correlation that 
+   * water-profile data can have to be considered good data.
+   */
+  private ByteBuffer lowCorrelationThreshold    = ByteBuffer.allocate(1);
+  
+  /**
+   * A field contains the number of beams used to calculate velocity data,
+   * not the number of physical beams. The Workhorse needs only three 
+   * beams to calculate water-current velocities.
+   */
+  private ByteBuffer numberOfBeams              = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains the number of depth cells over which the 
+   * Workhorse collects data. 
+   */
+  private ByteBuffer numberOfCells              = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains the number of code repetitions in the transmit pulse.
+   */
+  private ByteBuffer numberOfCodeRepetitions    = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that indicates whether the PD0 data are real or simulated.
+   *  The default is real data(0).
+   */
+  private ByteBuffer pdRealOrSimulatedFlag      = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains the minimum percentage of water-profiling pings 
+   * in an ensemble that must be considered good to output velocity data.
+   */
+  private ByteBuffer percentGoodMinimum         = ByteBuffer.allocate(1);
+  
+  /**
+   * pingMinutes, pingSeconds, and pingHundredths are fields that contains 
+   * the amount of time between ping groups in the ensemble.
+   */
+  private ByteBuffer pingHundredths             = ByteBuffer.allocate(1);
+  
+  /**
+   * pingMinutes, pingSeconds, and pingHundredths are fields that contains 
+   * the amount of time between ping groups in the ensemble.
+   */
+  private ByteBuffer pingMinutes                = ByteBuffer.allocate(1);
+  
+  /**
+   * pingMinutes, pingSeconds, and pingHundredths are fields that contains 
+   * the amount of time between ping groups in the ensemble.
+   */
+  private ByteBuffer pingSeconds                = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains the number of pings averaged together 
+   * during a data ensemble.
+   */
+  private ByteBuffer pingsPerEnsemble           = ByteBuffer.allocate(2);
+  
+  /**
+   * A field that contains the starting depth cell and the ending derpth
+   * cell used for water reference layer averaging.  See the manual for details.
+   */
+  private ByteBuffer referenceLayerAverage      = ByteBuffer.allocate(2);
+  
+  /**
+   * A field that reflects which sensors are available.  The bit pattern 
+   * is the same as listed for the sensorSource field.
+   */
+  private ByteBuffer sensorAvailability         = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains the selected source of environmental sensor data.
+   *  See the manual for interpreting each bit. 
+   */
+  private ByteBuffer sensorSource               = ByteBuffer.allocate(1);
+  
+  /**
+   *  A field that contains the instrument serial number (REMUS only),
+   *  and is considered a spare for all other Workhorse ADCPs.
+   */
+  private ByteBuffer serialNumber               = ByteBuffer.allocate(4);
+  
+  /**
+   * A field that contains the Signal Processing Mode.  
+   * This field will always be set to 1. 
+   */
+  private ByteBuffer signalProcessingMode       = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains the WB-command settings.  See the manual for details. 
+   */
+  private ByteBuffer systemBandwidth            = ByteBuffer.allocate(2);
+  
+  /**
+   *  A field that stores the Workhorse hardware configuration.  
+   *  See the manual for interpreting each bit. 
+   */
+  private ByteBuffer systemConfiguration        = ByteBuffer.allocate(2);
+  
+  /**
+    * A field that contains the CQ-command settings.  See the manual for details. 
+   */
+  private ByteBuffer systemPower                = ByteBuffer.allocate(1);
+  
+  /**
+   * A field that contains contains the distance between pulse repetitions.
+   */
+  private ByteBuffer transmitLagDistance        = ByteBuffer.allocate(2);
+  
+  /**
+   * A field that contains the length of the transmit pulse.
+   */
+  private ByteBuffer transmitPulseLength        = ByteBuffer.allocate(2);
+  
+  
+  
+  
+  /**
    *  Constructor.  This method populates the Fixed Leader fields from 
    *  the given ByteBuffer of data passed in as an argument, based on metadata 
    *  found in the EnsembleHeader.
