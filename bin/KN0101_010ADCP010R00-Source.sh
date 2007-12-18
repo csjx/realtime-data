efddaaf0c745521841eb7183cb60261e71efeaa9
Christopher Jones <csj@christopherjones.co>
2007-12-18 20:50:17 +0000

    I'm updating the ADCP driver script with parameters that are pre-configured for the Kilo Nalu array and the DataTurbine running on the Shore station server.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@122 9e959af2-1677-4b95-8350-622619c7fe3b

--- bin/KN0101_010ADCP010R00-Source.sh
+++ bin/KN0101_010ADCP010R00-Source.sh
@@ -10,15 +10,15 @@ $BBL_HOME/lib/turbine-3.7.0.jar:\
 $BBL_HOME/lib/log4j-1.2.8.jar:\
 $BBL_HOME/lib/log4j.properties
 
-# run the ADCPSource driver, connecting to the ADCP @ 192.168.1.101:2104
-# and to the RBNB server @ 192.168.1.103:3333, defining the source name as
+# run the ADCPSource driver, connecting to the ADCP @ 192.168.100.130:2101
+# and to the RBNB server @ 192.168.100.60:3333, defining the source name as
 # 'KN0101_010ADCP010R00' and the data channel as 'BinaryPD0EnsembleData'.  The 
-# client is also requesting a cache size of 10000 frames, and an archive size
-# of 100000 frames.  In this case, each frame is 1 ensemble transmitted by the
-# ADCP instrument, which equates to about 512 bytes to 1024 bytes of 
-# data per frame.
+# client is also requesting a cache size of 50000 frames, and an archive size
+# of 3000000 frames.  In this case, each frame is 1 ensemble transmitted by the
+# ADCP instrument, which equates to about 1024 bytes of # data per frame,
+# depending on the ADCP configuration.
 java edu.hawaii.soest.kilonalu.adcp.ADCPSource\
- -H 192.168.100.150\
+ -H 192.168.100.130\
  -P 2101\
  -S KN0101_010ADCP010R00 -C BinaryPD0EnsembleData\
  -s 192.168.100.60\
