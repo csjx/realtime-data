28047dfb7f3535bf95a4c585dc0a0a2c0657db99
Christopher Jones <csj@christopherjones.co>
2008-06-01 17:11:52 +0000

    I'm adding scripts to connect to the 20m 1200 kHz ADCP on the KiloNalu array, and am setting the connections scripts to use the localhost IP address as the RBNB server.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@214 9e959af2-1677-4b95-8350-622619c7fe3b

--- bin/KN0101_010ADCP010R00-Source.sh
+++ bin/KN0101_010ADCP010R00-Source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BBL_HOME=/home/cjones/development/bbl/trunk
+BBL_HOME=/usr/local/bbl/trunk
 export CLASSPATH=\
 $BBL_HOME/build/classes/:\
 $BBL_HOME/lib/rbnb.jar:\
@@ -10,9 +10,9 @@ $BBL_HOME/lib/turbine-3.9.0.jar:\
 $BBL_HOME/lib/log4j-1.2.8.jar:\
 $BBL_HOME/lib/log4j.properties
 
-# run the ADCPSource driver, connecting to the ADCP @ 192.168.100.130:2101
+# run the ADCPSource driver, connecting to the ADCP @ 192.168.100.139:2102
 # and to the RBNB server @ 192.168.100.60:3333, defining the source name as
-# 'KN0101_010ADCP010R00' and the data channel as 'BinaryPD0EnsembleData'.  The 
+# 'KN02XX_020ADCP020R00' and the data channel as 'BinaryPD0EnsembleData'.  The 
 # client is also requesting a cache size of 50000 frames, and an archive size
 # of 3000000 frames.  In this case, each frame is 1 ensemble transmitted by the
 # ADCP instrument, which equates to about 1024 bytes of # data per frame,
@@ -21,7 +21,7 @@ java edu.hawaii.soest.kilonalu.adcp.ADCPSource\
  -H 192.168.100.130\
  -P 2101\
  -S KN0101_010ADCP010R00 -C BinaryPD0EnsembleData\
- -s 192.168.100.60\
+ -s 192.168.103.50\
  -p 3333\
  -z 50000\
  -Z 13000000
--- bin/KN02XX_020ADCP020R00-Convert.sh
+++ bin/KN02XX_020ADCP020R00-Convert.sh
@@ -11,17 +11,13 @@ $BBL_HOME/lib/log4j-1.2.8.jar:\
 $BBL_HOME/lib/log4j.properties
 
 # run the FileArchiverSink client, connecting to the RBNB server 
-# @ 127.0.0.1:3333, reading from the source named 'KN0101_010ADCP010R00' and the 
+# @ 127.0.0.1:3333, reading from the source named 'KN02XX_020ADCP020R00' and the 
 # data channel 'BinaryPD0EnsembleData'.  The client is also setting the sink 
-# name to be 'KN0101_0101ADCP010R00_BinaryPD0EnsembleData_FileArchiver', and is
-# setting the archive directory to be '
-# '/data1/rbnb/KN0101_0101ADCP010R00/BinaryPD0EnsembleData'.  The sink client
-# connects to the server once per hour, every hour, and archives the previous
-# hour's worth of data collected.
+# name to be 'KN02XX_020ADCP020R00_BinaryPD0EnsembleData_Converter'.
 
 java edu.hawaii.soest.kilonalu.utilities.TextOutputPlugin\
  -s 127.0.0.1\
  -p 3333\
- -n KN0101_010ADCP010R00\
+ -n KN02XX_020ADCP020R00\
  -c BinaryPD0EnsembleData\
- -k KN0101_010ADCP010R00_BinaryPD0EnsembleData_Converter
+ -k KN02XX_020ADCP020R00_BinaryPD0EnsembleData_Converter
