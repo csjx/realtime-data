d57c7e109a09f4c1780f748861a9f7da29834b93
Christopher Jones <csj@christopherjones.co>
2008-06-14 18:49:54 +0000

    I've reduced the requested cache bytes to 50,000 since the bbl server only has 3.5GB of RAM
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@224 9e959af2-1677-4b95-8350-622619c7fe3b

--- bin/KN02XX_020ADCP020R00-Source.sh
+++ bin/KN02XX_020ADCP020R00-Source.sh
@@ -14,14 +14,15 @@ $BBL_HOME/lib/log4j.properties
 # and to the RBNB server @ 192.168.100.60:3333, defining the source name as
 # 'KN02XX_020ADCP020R00' and the data channel as 'BinaryPD0EnsembleData'.  The 
 # client is also requesting a cache size of 50000 frames, and an archive size
-# of 3000000 frames.  In this case, each frame is 1 ensemble transmitted by the
-# ADCP instrument, which equates to about 1024 bytes of # data per frame,
-# depending on the ADCP configuration.
+# of 31536000 frames.  In this case, each frame is 1 ensemble transmitted by the
+# ADCP instrument, which equates to about 1024 bytes of data per frame,
+# depending on the ADCP configuration. With this ring buffer request, this source
+# use approximately 50MB od RAM, and 32GB of disk storage (in a 1 year period).
 java edu.hawaii.soest.kilonalu.adcp.ADCPSource\
  -H 192.168.100.139\
  -P 2102\
  -S KN02XX_020ADCP020R00 -C BinaryPD0EnsembleData\
- -s 192.168.103.50\
+ -s 192.168.100.60\
  -p 3333\
  -z 50000\
- -Z 13000000
+ -Z 31536000
