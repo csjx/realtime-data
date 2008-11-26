bfa2ad8131829521899b7773872864021671a394
Christopher Jones <csj@christopherjones.co>
2008-11-26 23:36:18 +0000

    I've made some minor documentation changes in the script.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@243 9e959af2-1677-4b95-8350-622619c7fe3b

--- bin/KN0101_010SBEX010R00-Source.sh
+++ bin/KN0101_010SBEX010R00-Source.sh
@@ -10,14 +10,14 @@ $BBL_HOME/lib/turbine-3.9.0.jar:\
 $BBL_HOME/lib/log4j-1.2.8.jar:\
 $BBL_HOME/lib/log4j.properties
 
-# run the SeabirdCTDSource driver, connecting to the CTD @ 68.25.65.111:5111
+# run the SBE37Source driver, connecting to the CTD @ 192.168.100.136:2103
 # and to the RBNB server @ bbl.ancl.hawaii.edu:3333, defining the source name as
-# 'AW01XX_002CTDXXXXR00' and the data channel as 'BinaryPD0EnsembleData'.  The 
-# client is also requesting a cache size of 750000 frames, and an archive size
+# 'KN0101_010SBEX010R00' and the data channel as 'DecimalASCIISampleData'.  The 
+# client is also requesting a cache size of 375000 frames, and an archive size
 # of 7884000 frames.  In this case, each frame is 1 sample transmitted by the
-# CTD instrument (15/minute), which equates to about 69 bytes of data per frame,
+# CTD instrument (4/minute), which equates to about 101 bytes of data per frame,
 # depending on the CTD configuration. With this ring buffer request, this source
-# use approximately 25MB of RAM, and 543MB of disk storage (in a 1 year period).
+# use approximately 37.8MB of RAM, and 796MB of disk storage (in a 1 year period).
 java edu.hawaii.soest.kilonalu.ctd.SBE37Source\
  -H 192.168.100.136\
  -P 2103\
