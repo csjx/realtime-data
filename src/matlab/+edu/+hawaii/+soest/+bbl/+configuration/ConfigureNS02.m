c379884169701169c5a9eeaf5a5c0ea28f09c69e
Christopher Jones <csj@christopherjones.co>
2008-12-18 20:14:19 +0000

    I'm adding in separate configuration classes for the 2 Ala Wai CTD sensors, NS01 and NS02.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@247 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ src/matlab/+edu/+hawaii/+soest/+bbl/+configuration/ConfigureNS02.m
@@ -0,0 +1,92 @@
+%  Copyright: 2007 Regents of the University of Hawaii and the
+%             School of Ocean and Earth Science and Technology
+% 
+%    Purpose: To provide configuration information for near real-time
+%             data processors, such as the CTDProcessor class.
+%    Authors: Christopher Jones             
+%  
+% $HeadURL: $
+% $LastChangedDate: $
+% $LastChangedBy: $
+% $LastChangedRevision: $
+% 
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+% 
+
+% The Configure class provides runtime configuration information for use by
+% other classes that process near real-time data, such as the CTDProcessor class.
+classdef ConfigureNS02
+  properties
+    % A boolean property that enables or disables debugging 
+    debug = true;
+    
+    % The platform specific path separator 
+    pathSeparator = '/';
+    
+    % The source code base directory
+    baseDirectory = '/usr/local/bbl/trunk/'; % note trailing slash
+    
+    % The output path directory
+    outputDirectory = '/var/www/html/OE/KiloNalu/Data/CTD/NS02/'; % note trailing slash
+    
+    % The path of the RBNB software
+    rbnbPath = '/usr/local/RBNB/V3.1B4a/'; % note trailing slash
+    
+    % The path of the RBNB java library
+    rbnbLibraryPath = '/usr/local/RBNB/V3.1B4a/bin/rbnb.jar';
+    
+    % The path of the RBNB java library
+    rbnbMatlabPath = '/usr/local/RBNB/V3.1B4a/Matlab/'; % note trailing slash
+    
+    % The name of the RBNB Data Turbine server
+    rbnbServer = '127.0.0.1';
+    
+    % The port of the RBNB Data Turbine server
+    rbnbPort = '3333';
+
+    % The name of the sink client connecting to the RBNB Data Turbine server
+    rbnbSinkName = 'MatlabNS02CTDProcessingSink';
+    
+    % The name of the desired data source
+    rbnbSource = 'AW02XX_001CTDXXXXR00';
+    
+    % The name of the desired data channel
+    rbnbChannel = 'DecimalASCIISampleData';
+    
+    % The duration of the channel data query
+    duration = 604800; % 60 sec x 60 min x 24 hr x 7 days
+    
+    % The time reference of the channel data query
+    reference = 'newest';
+    
+    % The start time channel data query
+    startTime = 0;
+    
+    % The timer interval for scheduled processing in minutes
+    timerInterval = 20;
+
+  end % properties
+  
+  methods
+
+    % The Constructor: creates an instance of the Configure object, and sets
+    % the pertinent software library paths
+    function self = ConfigureNS02()
+      % set the library paths based on the configuration properties 
+    end % Configure
+
+  end % methods
+  
+end % classdef
