154f353830388db66314430bcb4f2291c9fa68ff
Christopher Jones <csj@christopherjones.co>
2009-05-03 04:15:51 +0000

    I'm modifying the instrument start scripts so they append to the source status logs rather than overwrite them each time the script is called.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@346 9e959af2-1677-4b95-8350-622619c7fe3b

--- bin/Start-KN0201_020TCHNXXXR00.sh
+++ bin/Start-KN0201_020TCHNXXXR00.sh
@@ -7,7 +7,7 @@ bblHome="/usr/local/bbl/trunk";
 
 # start the instrument driver
 cd $bblHome;
-./bin/$sourceName-Source.sh > /var/log/rbnb/$sourceName-Source.log 2>&1 &
+./bin/$sourceName-Source.sh >> /var/log/rbnb/$sourceName-Source.log 2>&1 &
 
 # tail the log file to confirm the driver is running
 echo -e "\nStarted $sourceString driver\n";
