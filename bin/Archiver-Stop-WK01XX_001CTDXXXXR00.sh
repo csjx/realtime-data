fe4f701b2391dac936b643b3865c030d4896cc04
Christopher Jones <csj@christopherjones.co>
2009-06-05 00:51:55 +0000

    For the NS03 CTD, I'm adding in the convenience scripts for starting and stopping the file archiver sink.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@366 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/Archiver-Stop-WK01XX_001CTDXXXXR00.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set up the variables
+sourceName="WK01XX_001CTDXXXXR00";
+sinkType="FileArchiverSink";
+sourceString="1 meter Waikiki NS03 CTD";
+
+# find any pertinent processes
+processDetails=$(ps -ef | grep $sinkType | grep $sourceName);
+processCount=$(echo $processDetails | grep -ve "^$" | wc -l);
+
+# kill the archiver process if it exists
+if [ $processCount == 1 ]
+then
+  kill -15 `ps -ef | grep $sinkType | grep $sourceName | tr -s ' ' ' ' | cut -d" " -f2`;
+  sleep 1;
+  echo "Stopped the $sinkType driver connected to the $sourceString.";
+else
+  echo "There are no running $sinkType archivers to stop.";
+fi
