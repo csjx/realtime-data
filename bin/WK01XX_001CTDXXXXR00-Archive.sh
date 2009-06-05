31b713b2516e4ce4bc08feceb7b3d41c9d015b3d
Christopher Jones <csj@christopherjones.co>
2009-06-05 00:05:46 +0000

    These are script additions for the Alawai and Waikiki CTDs (Starting, Stopping, Archiving).
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@365 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/WK01XX_001CTDXXXXR00-Archive.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BBL_HOME=/usr/local/bbl/trunk
+export CLASSPATH=\
+$BBL_HOME/build/classes/:\
+$BBL_HOME/lib/rbnb.jar:\
+$BBL_HOME/lib/commons-codec-1.3.jar:\
+$BBL_HOME/lib/commons-cli-1.0.jar:\
+$BBL_HOME/lib/commons-logging-1.0.4.jar:\
+$BBL_HOME/lib/turbine-3.9.0.jar:\
+$BBL_HOME/lib/log4j-1.2.8.jar:\
+$BBL_HOME/lib/log4j.properties
+
+# run the FileArchiverSink client, connecting to the RBNB server 
+# @ 127.0.0.1:3333, reading from the source named 'WK01XX_001CTDXXXXR00' and the 
+# data channel 'BinaryPD0EnsembleData'.  The client is also setting the sink 
+# name to be 'WK01XX_001CTDXXXXR00_DecimalASCIIData_FileArchiver', and is
+# setting the archive directory to be '
+# '/data/rbnb/WK01XX_001CTDXXXXR00/DecimalASCIISampleData'.  The sink client
+# connects to the server once per hour, every hour, and archives the previous
+# hour's worth of data collected.
+
+java edu.hawaii.soest.kilonalu.utilities.FileArchiverSink\
+ -s 192.168.103.50\
+ -p 3333\
+ -n WK01XX_001CTDXXXXR00\
+ -c DecimalASCIISampleData\
+ -k WK01XX_001CTDXXXXR00_DecimalASCIIData_FileArchiver\
+ -d /data/raw/alawai/WK01XX_001CTDXXXXR00/DecimalASCIISampleData
