d7e0a5c7b9d2e6e5509bee9a066e885d77e4fa6c
Christopher Jones <csj@christopherjones.co>
2009-11-06 23:54:25 +0000

    I'm adding in two logging setup methods:
    getLogConfigurationFile(), which returns the log configuration file location as a string
    
    setLogConfigurationFile(), which sets the log configuration file location.
    
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@472 9e959af2-1677-4b95-8350-622619c7fe3b

--- src/java/edu/hawaii/soest/kilonalu/ctd/CTDConverter.java
+++ src/java/edu/hawaii/soest/kilonalu/ctd/CTDConverter.java
@@ -112,4 +112,24 @@ public class CTDConverter {
     this.ctdParser = ctdParser;                           
     
   }
+
+  /**
+   * A method that gets the log configuration file location
+   *
+   * @return logConfigurationFile  the log configuration file location
+   */
+  public String getLogConfigurationFile() {
+    return this.logConfigurationFile;
+  }
+
+  /**
+   * A method that sets the log configuration file name
+   *
+   * @param logConfigurationFile  the log configuration file name
+   */
+  public void setLogConfigurationFile(String logConfigurationFile) {
+    this.logConfigurationFile = logConfigurationFile;
+  }
+
+  
 }                                               
