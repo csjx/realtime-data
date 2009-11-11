7d3dafff9ced36e8aa95464bf37f5acca0c34c46
Christopher Jones <csj@christopherjones.co>
2009-11-11 00:08:37 +0000

    I've added the CTDParser class, which represents a sample of data produced by a Seabird Seacat 19plus CTD Profiler.  The parser is intended to parse one of the four OUTPUTFORMATs described in the manual.  Therefore, the parser will handle both Hex and Decimal data formats, as well as data from both profile and moored modes. This class includes fields for each of the stated  fields in the sample format documentation, along with getter and setter methods for accessing those fields.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@481 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ src/java/edu/hawaii/soest/kilonalu/ctd/CTDParser.java
@@ -0,0 +1,77 @@
+/**
+ *  Copyright: 2009 Regents of the University of Hawaii and the
+ *             School of Ocean and Earth Science and Technology
+ *    Purpose: A class that represents a data sample of data produced by
+ *             a Seabird Seacat 19plus CTD profiler as described in
+ *            the SBE 19plus SEACAT Profiler User's Manual 
+ *            (Manual Version #010, 01/02/03 ).  The parser is intended to parse
+ *            one of the four OUTPUTFORMATs described in the manual.
+ *
+ *   Authors: Christopher Jones
+ *
+ * $HeadURL: https://bbl.ancl.hawaii.edu/projects/bbl/trunk/src/java/edu/hawaii/soest/kilonalu/ctd/CTDParser.java $
+ * $LastChangedDate: 2009-06-23 19:29:51 -0600 (Tue, 23 Jun 2009) $
+ * $LastChangedBy: cjones $
+ * $LastChangedRevision: 404 $
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
+package edu.hawaii.soest.kilonalu.ctd;
+import java.math.BigInteger;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.StringTokenizer;
+import java.util.TimeZone;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.DecoderException;
+
+import org.apache.commons.math.linear.Array2DRowRealMatrix;
+import org.apache.commons.math.linear.RealMatrix;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ *  A class that represents a sample of data produced by
+ *  a Seabird Seacat 19plus CTD Profiler.  The parser is intended to parse one
+ *  of the four OUTPUTFORMATs described in the manual.  Therefore, the parser
+ *  will handle both Hex and Decimal data formats, as well as data from both 
+ *  profile and moored modes. This class includes fields for each of the stated
+ *   fields in the sample format documentation, along with getter and setter
+ *  methods for accessing those fields.
+ */
+public class CTDParser {
+
+  
+}                                               
