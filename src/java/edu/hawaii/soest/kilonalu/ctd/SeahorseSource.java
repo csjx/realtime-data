704ec725149d80c0a1ddc79f7ac7d05d398675ec
Christopher Jones <csj@christopherjones.co>
2009-11-14 18:06:07 +0000

    I've added in the SeahorseSource class used to harvest a compressed hexadecimal ASCII data file from a Brooke Ocean Seahorse profiler equipped with a Seacat SBE19plus CTD. Communication is over a TCP socket connection with an Iridium satellite modem and a serial2ip converter host. The data samples are then converted into RBNB frames and pushed into the RBNB DataTurbine real time server. This class extends org.nees.rbnb.RBNBSource, which in turn extends org.nees.rbnb.RBNBBase, and therefore follows the API conventions found in the org.nees.rbnb code.
    The parsing of the data stream relies on the premise that each sample (scan) of data is a Hex-encoded string of values, and that each sample is terminated by a newline character (\n). It is also assumed that the sample rate is 4Hz, or a sample is taken every 15 seconds.
    
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@504 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ src/java/edu/hawaii/soest/kilonalu/ctd/SeahorseSource.java
@@ -0,0 +1,80 @@
+/**
+ *  Copyright: 2007 Regents of the University of Hawaii and the
+ *             School of Ocean and Earth Science and Technology
+ *    Purpose: To convert a Seacat ASCII data source into RBNB Data Turbine
+ *             frames for archival and realtime access.
+ *    Authors: Christopher Jones
+ *
+ * $HeadURL: $
+ * $LastChangedDate: $
+ * $LastChangedBy: cjones $
+ * $LastChangedRevision:  $
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
+package edu.hawaii.soest.kilonalu.ctd;
+
+import com.rbnb.sapi.ChannelMap;
+import com.rbnb.sapi.Source;
+import com.rbnb.sapi.SAPIException;
+
+import java.lang.StringBuffer;
+
+import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import java.util.zip.Inflater;
+import java.util.zip.DataFormatException;
+
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+
+import org.apache.commons.codec.binary.Hex;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+import org.nees.rbnb.RBNBBase;
+import org.nees.rbnb.RBNBSource;
+
+/**
+ * A simple class used to harvest a compressed hexadecimal ASCII data file 
+ * from a Brooke Ocean Seahorse profiler equipped with a Seacat SBE19plus CTD. 
+ * Communication is over a TCP socket connection with an Iridium satellite modem
+ * and a serial2ip converter host. The data samples are then converted into RBNB
+ * frames and pushed into the RBNB DataTurbine real time server.  This class  
+ * extends org.nees.rbnb.RBNBSource, which in turn extends org.nees.rbnb.RBNBBase, 
+ * and therefore follows the API conventions found in the org.nees.rbnb code.  
+ *
+ * The parsing of the data stream relies on the premise that each sample (scan) 
+ * of data is a Hex-encoded string of values, and that each sample is terminated
+ * by a newline character (\n).  It is also assumed that the sample rate is 4Hz,
+ * or a sample is taken every 15 seconds.
+ *
+ */
+public class SeahorseSource extends RBNBSource {
+
+
+}
