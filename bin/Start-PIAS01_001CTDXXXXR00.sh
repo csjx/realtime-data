24632b680fdeb0491fa0a37317d41ea6b542e9ad
Christopher Jones <csj@christopherjones.co>
2010-04-28 21:37:57 +0000

    I've added Start scripts for the Marshall Islands, Palau, American Samoa, and Micronesia CTD deployments.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@620 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/Start-PIAS01_001CTDXXXXR00.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# set up the variables
+sourceName="PIAS01_001CTDXXXXR00";
+sourceType="CTDSource";
+sourceString="1 meter PIAS CTD";
+bblHome="/usr/local/bbl/trunk";
+
+# start the instrument driver
+cd $bblHome;
+./bin/$sourceName-Source.sh >> /var/log/rbnb/$sourceName-Source.log 2>&1 &
+
+# tail the log file to confirm the driver is running
+echo -e "\nStarted $sourceString driver\n";
+echo -e "Starting to view the $sourceString streaming log.\n"; 
+
+for i in $(seq 1 1 3)
+do # flash instructions on stopping the tail process
+ echo -en "\r** Type Control-C to stop viewing the log. **"; 
+ sleep 1;
+ echo -en "\r   Type Control-C to stop viewing the log.   "; 
+ sleep 1;
+done
+echo -e "\n";
+
+# start tailing the log file
+tail -f /var/log/rbnb/$sourceName-Source.log; 
