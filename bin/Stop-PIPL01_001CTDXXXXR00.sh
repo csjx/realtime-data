0b4c9826b60698461a5164bd7bd379bd7f624761
Christopher Jones <csj@christopherjones.co>
2010-04-28 21:44:11 +0000

    I've added Stop scripts for the Marshall Islands, Palau, American Samoa, and Micronesia CTD deployments.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@621 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/Stop-PIPL01_001CTDXXXXR00.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set up the variables
+sourceName="PIPL01_001CTDXXXXR00";
+sourceType="CTDSource";
+sourceString="1 meter PIPL CTD";
+
+# find any pertinent processes
+processDetails=$(ps -ef | grep $sourceType | grep $sourceName);
+processCount=$(echo $processDetails | grep -ve "^$" | wc -l);
+
+# kill the driver process if it exists
+if [ $processCount == 1 ]
+then
+  kill -15 `ps -ef | grep $sourceType | grep $sourceName | tr -s ' ' ' ' | cut -d" " -f2`;
+  sleep 1;
+  echo "Stopped the $sourceType driver connected to the $sourceString.";
+else
+  echo "There are no running $sourceType drivers to stop.";
+fi
