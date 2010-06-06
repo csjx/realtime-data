6271a1b177607535e2235858f5d0a9c35c51793c
Christopher Jones <csj@christopherjones.co>
2010-06-06 14:10:19 +0000

    I'm adding the execute script for the HIOOS Water Quality Buoys, which runs the StorXDispatcher class which in turn calls the StorXSource classes defined in the email.account.properties.xml file.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@658 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/HIWQXX_XXXCTDXXXXXXX-Source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#BBL_HOME=/usr/local/bbl/trunk;
+BBL_HOME=/Users/cjones/Documents/Development/bbl/trunk;
+export CLASSPATH=\
+$BBL_HOME/build/classes/:\
+$BBL_HOME/lib/rbnb.jar:\
+$BBL_HOME/lib/mail.jar:\
+$BBL_HOME/lib/commons-codec-1.3.jar:\
+$BBL_HOME/lib/commons-cli-1.0.jar:\
+$BBL_HOME/lib/commons-logging-1.0.4.jar:\
+$BBL_HOME/lib/commons-configuration-1.6.jar:\
+$BBL_HOME/lib/commons-collections-3.2.1.jar:\
+$BBL_HOME/lib/commons-lang-2.4.jar:\
+$BBL_HOME/lib/log4j-1.2.8.jar:\
+$BBL_HOME/lib/log4j.properties
+
+java edu.hawaii.soest.kilonalu.ctd.StorXDispatcher\
+ -s 127.0.0.1\
+ -p 3333
