cddcca5759214f8f73e1e0a5627237739bc9f65a
Christopher Jones <csj@christopherjones.co>
2010-06-23 17:08:55 +0000

    Due to problems with the RXTX serial connection on the gumstix, I've modified the PacIOOS deployments to now use a FileSource class to read data into the DataTurbine from an onboard file which is appended to by a serial terminal program (minicom).  The PIAS01 execution script now reflects the driver change.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@686 9e959af2-1677-4b95-8350-622619c7fe3b

--- bin/PIAS01_001CTDXXXXR00-Source.sh
+++ bin/PIAS01_001CTDXXXXR00-Source.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-BBL_HOME=/usr/local/bbl/trunk;
+#BBL_HOME=/usr/local/bbl/trunk;
+BBL_HOME=/Users/cjones/Documents/Development/bbl/trunk;
 export CLASSPATH=\
 $BBL_HOME/build/classes/:\
 $BBL_HOME/lib/rbnb.jar:\
-$BBL_HOME/lib/RXTXcomm.jar:\
 $BBL_HOME/lib/commons-codec-1.3.jar:\
 $BBL_HOME/lib/commons-cli-1.0.jar:\
 $BBL_HOME/lib/commons-logging-1.0.4.jar:\
 $BBL_HOME/lib/log4j-1.2.8.jar:\
 $BBL_HOME/lib/log4j.properties;
 
-export IFACE=wlan0;
-export IP=$(ip addr show $IFACE | grep "inet " | tr -s " " " " | cut -d" " -f3 | cut -d"/" -f1);
 export SOURCE="PIAS01_001CTDXXXXR00";
 
-# run the CTDSource driver, connecting to the CTD the /dev/ttyUSB0 serial port
+# run the FileSource driver, opening the data file at /data/spool/PIAS01_001CTDXXXXR00.log
 # and to the RBNB server @ bbl.ancl.hawaii.edu:3333, defining the source name as
 # 'PIAS01_001CTDXXXXR00' and the data channel as 'DecimalASCIISampleData'.  The 
 # client is also requesting a cache size of 126000 frames, and an archive size
@@ -22,13 +20,42 @@ export SOURCE="PIAS01_001CTDXXXXR00";
 # CTD instrument (15/minute), which equates to about 65 bytes of data per frame,
 # depending on the CTD configuration. With this ring buffer request, this source
 # use approximately 2MB of RAM, and 126MB of disk storage (in a 90 day period).
-java edu.hawaii.soest.kilonalu.ctd.CTDSource\
- -t serial\
- -o xml\
- -c /dev/ttyUSB0\
+
+# The regular expression used to match data lines as opposed to other 
+# instrument messages is:
+# " *.*, *.*, *.*, *.*, *.*, *.*, *\d{2} [A-Z][a-z][a-z] *\d{4} *\d{2}:\d{2}:\d{2}\s*"
+#
+# which corresponds to a data line, e.g.:
+# " 29.2616,  5.73958,    1.666, 0.0681, 0.0803,  34.8810, 09 Jun 2010 09:59:21\r\n"
+#
+# Each section is explained below with single quotes around each section:
+# ' *.*,'            - match any # of spaces followed by any # of characters, 
+#                      followed by a comma
+# ' *.*,'            - match any # of spaces followed by any # of characters, 
+#                      followed by a comma
+# ' *.*,'            - match any # of spaces followed by any # of characters, 
+#                      followed by a comma
+# ' *.*,'            - match any # of spaces followed by any # of characters, 
+#                      followed by a comma
+# ' *.*,'            - match any # of spaces followed by any # of characters, 
+#                      followed by a comma
+# ' *.*,'            - match any # of spaces followed by any # of characters, 
+#                      followed by a comma
+# ' *\d{2}'          - match any # of spaces followed by two digits
+# ' [A-Z][a-z][a-z]' - match a space followed by an uppercase character,
+#                      followed by two lowercase characters
+# ' *\d{4}'          - match any # of spaces followed by four digits
+# ' *\d{2}'          - match any # of spaces followed by two digits
+# ':\d{2}'           - match a colon followed by two digits
+# ':\d{2}'           - match a colon followed by two digits
+# '\s*'              - match any number of whitespace characters (e.g. \r or \n)
+
+java edu.hawaii.soest.kilonalu.utilities.FileSource\
+ -F "/data/spool/PIAS01_001CTDXXXXR00.log"\
+ -e " *.*, *.*, *.*, *.*, *.*, *.*, *\d{2} [A-Z][a-z][a-z] *\d{4} *\d{2}:\d{2}:\d{2}\s*"\
  -S $SOURCE\
  -C DecimalASCIISampleData\
- -s $IP\
+ -s 127.0.0.1\
  -p 3333\
  -z 126000\
  -Z 7776000
