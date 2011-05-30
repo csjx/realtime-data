3246bb6fdf3d2f7e4bbece25dc38df1f6dd60802
Christopher Jones <csj@christopherjones.co>
2011-05-30 03:27:40 +0000

    Created the script using the original NS02 script as a template. Modified the script name, IP and port number.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@880 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/MB01XX_001CTDXXXXR00-Source.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BBL_HOME=/usr/local/bbl/trunk
+export CLASSPATH=\
+$BBL_HOME/build/classes/:\
+$BBL_HOME/lib/rbnb.jar:\
+$BBL_HOME/lib/commons-codec-1.3.jar:\
+$BBL_HOME/lib/commons-cli-1.0.jar:\
+$BBL_HOME/lib/commons-logging-1.0.4.jar:\
+$BBL_HOME/lib/log4j-1.2.8.jar:\
+$BBL_HOME/lib/log4j.properties
+
+# run the SeabirdCTDSource driver, connecting to the CTD @ 24.221.193.197:5111
+# and to the RBNB server @ bbl.ancl.hawaii.edu:3333, defining the source name as
+# 'MB01XX_001CTDXXXXR00' and the data channel as 'BinaryPD0EnsembleData'.  The 
+# client is also requesting a cache size of 50000 frames, and an archive size
+# of 31536000 frames.  In this case, each frame is 1 sample transmitted by the
+# CTD instrument (15/minute), which equates to about 69 bytes of data per frame,
+# depending on the CTD configuration. With this ring buffer request, this source
+# use approximately 25MB of RAM, and 543MB of disk storage (in a 1 year period).
+java edu.hawaii.soest.kilonalu.ctd.CTDSource\
+ -H 24.221.193.197\
+ -P 5111\
+ -S MB01XX_001CTDXXXXR00\
+ -C DecimalASCIISampleData\
+ -s 192.168.103.50\
+ -p 3333\
+ -z 50000\
+ -Z 31536000
