88d9dea23da69984610342ba0735579c2aaaee25
Christopher Jones <csj@christopherjones.co>
2011-09-06 18:48:09 +0000

    Scripts for the CINM driver.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@892 9e959af2-1677-4b95-8350-622619c7fe3b

---
+++ bin/Start-PINM01_002CTDX008R00.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# set up the variables
+sourceName="PINM01_001CTDXXXXR00";
+sourceType="FileSource";
+sourceString="1 meter PINM01 CTD";
+bblHome=".";
+logDir="./log/";
+# start the instrument driver
+cd $bblHome;
+./bin/$sourceName-Source.sh >> $logDir$sourceName-Source.log 2>&1 &
+
+# tail the log file to confirm the driver is running
+echo -e "\nStarted $sourceString driver\n";
+echo -e "Starting to view the $sourceString streaming log.\n"; 
+
+for i in $(seq 1 1 2)
+do # flash instructions on stopping the tail process
+ echo -en "\r** Type Control-C to stop viewing the log. **"; 
+ sleep 1;
+ echo -en "\r   Type Control-C to stop viewing the log.   "; 
+ sleep 1;
+done
+echo -e "\n";
+
+# start tailing the log file
+#tail -f $logDir$sourceName-Source.log; 
