bf84557f18cf342508c16d7efce57faaa2005087
Christopher Jones <csj@christopherjones.co>
2013-08-15 15:18:36 +0000

    Add minor debugging statement and license.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@1053 9e959af2-1677-4b95-8350-622619c7fe3b

--- src/test/edu/hawaii/soest/pacioos/text/MockDataSource.java
+++ src/test/edu/hawaii/soest/pacioos/text/MockDataSource.java
@@ -1,6 +1,30 @@
 /**
- * 
- */
+ *  Copyright: 2013 Regents of the University of Hawaii and the
+ *             School of Ocean and Earth Science and Technology
+ *    Purpose: A class that acts as a mock data instrument, streaming data over TCP when a client
+ *    connects.
+ *
+ *   Authors: Christopher Jones
+ *
+ * $HeadURL: $
+ * $LastChangedDate: $
+ * $LastChangedBy:  $
+ * $LastChangedRevision:  $
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
 package edu.hawaii.soest.pacioos.text;
 
 import java.io.File;
@@ -12,6 +36,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -85,8 +110,8 @@ public class MockDataSource implements Runnable {
 			    out = new PrintWriter(clientSocket.getOutputStream(), true);
 				// loop through the file and send each line over the wire
 				for ( String line : lines ) {
-						line = "\r\n" + line + "\r\n";
-			    	//log.debug("Sending " + line);
+					line = "\r\n" + line + "\r\n";
+			    	log.debug("Line bytes: " + new String(Hex.encodeHex(line.getBytes("US-ASCII"))));
 			    	out.println(line);
 			    	Thread.sleep(1000);
 			    }
