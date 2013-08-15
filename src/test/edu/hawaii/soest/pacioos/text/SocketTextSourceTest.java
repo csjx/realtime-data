c174e0af71169fe6e2007dfc74924752ba66f43c
Christopher Jones <csj@christopherjones.co>
2013-08-15 15:19:58 +0000

    Add minor debugging statements and license text.
    git-svn-id: https://bbl.ancl.hawaii.edu/projects/bbl/trunk@1054 9e959af2-1677-4b95-8350-622619c7fe3b

--- src/test/edu/hawaii/soest/pacioos/text/SocketTextSourceTest.java
+++ src/test/edu/hawaii/soest/pacioos/text/SocketTextSourceTest.java
@@ -1,6 +1,29 @@
 /**
- * 
- */
+ *  Copyright: 2013 Regents of the University of Hawaii and the
+ *             School of Ocean and Earth Science and Technology
+ *    Purpose: A test class that exercises the SocketTextSource class.
+ *
+ *   Authors: Christopher Jones
+ *
+ * $HeadURL: $
+ * $LastChangedDate: $
+ * $LastChangedBy:  $
+ * $LastChangedRevision:  $
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
 package edu.hawaii.soest.pacioos.text;
 
 import static org.junit.Assert.*;
@@ -8,7 +31,6 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.Socket;
 import java.util.Properties;
 
 import org.apache.commons.configuration.ConfigurationException;
@@ -84,9 +106,15 @@ public class SocketTextSourceTest {
 	public void tearDown() throws Exception {
 		// shut down the DT
 		log.info("Stopping the mock DataTurbine");
-		mockDT.stop();
+		if ( mockDT != null ) {
+			mockDT.stop();
+			
+		}
 		// shut down the data source
-		mockDataSource.stop();
+		if ( mockDataSource != null ) {			
+			mockDataSource.stop();
+			
+		}
 	}
 
 	/**
@@ -116,8 +144,9 @@ public class SocketTextSourceTest {
 			socketTextSource.stop();
 			
 		} catch (ConfigurationException e) {
-			fail("Couldn't configure a driver using " + configLocation);
+			log.error(e.getMessage());
 			e.printStackTrace();
+			fail("Couldn't configure a driver using " + configLocation);
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
