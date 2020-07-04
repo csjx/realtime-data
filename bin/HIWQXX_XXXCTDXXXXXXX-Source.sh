9d15cc4808892501ff2b2a1171948d03abf43f95
Christopher Jones <csj@christopherjones.co>
2020-07-04 14:16:25 -0600

    Remove older scripts used to start and stop instrument drivers.

--- bin/HIWQXX_XXXCTDXXXXXXX-Source.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export CLASSPATH=$REALTIME_DATA/realtime-data-1.1.0-jar-with-dependencies.jar;
-
-java edu.hawaii.soest.hioos.storx.StorXDispatcher\
- -s realtime.pacioos.hawaii.edu\
- -p 3333
