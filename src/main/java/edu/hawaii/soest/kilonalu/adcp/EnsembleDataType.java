bd1a0b335dbd663d4485fe0b90e44020d8adcdd8
Christopher Jones <csj@christopherjones.co>
2020-09-18 17:29:40 -0600

    Minor formatting changes.

--- src/main/java/edu/hawaii/soest/kilonalu/adcp/EnsembleDataType.java
+++ src/main/java/edu/hawaii/soest/kilonalu/adcp/EnsembleDataType.java
@@ -1,16 +1,11 @@
-/**
+/*
  *  Copyright: 2007 Regents of the University of Hawaii and the
  *             School of Ocean and Earth Science and Technology
  *    Purpose: An enumerated type that represents the data types produced by
  *            an RDI 1200kHz Workhorse Acoustic Doppler Current Profiler in the
- *            default PD0 format as described in RDI's "Workhorse Commands and 
+ *            default PD0 format as described in RDI's "Workhorse Commands and
  *            Output Data Format" manual, P/N 957-6156-00 (March 2005)
  *   Authors: Christopher Jones
- *
- * $HeadURL$
- * $LastChangedDate$
- * $LastChangedBy$
- * $LastChangedRevision$
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,24 +20,24 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */ 
+ */
 package edu.hawaii.soest.kilonalu.adcp;
 
 /**
- * An enumerated type that represents the list of data types produced by 
+ * An enumerated type that represents the list of data types produced by
  * an RDI 1200kHz Workhorse Acoustic Doppler Current Profiler in the
- * default PD0 format 
+ * default PD0 format
  */
 public enum EnsembleDataType {
-    
-    HEADER               , //represents data type ID 0x7F7F
-    FIXED_LEADER         , //represents data type ID 0x0000
-    VARIABLE_LEADER      , //represents data type ID 0x8000
-    VELOCITY_PROFILE     , //represents data type ID 0x0001
-    CORRELATION_PROFILE  , //represents data type ID 0x0002
+
+    HEADER, //represents data type ID 0x7F7F
+    FIXED_LEADER, //represents data type ID 0x0000
+    VARIABLE_LEADER, //represents data type ID 0x8000
+    VELOCITY_PROFILE, //represents data type ID 0x0001
+    CORRELATION_PROFILE, //represents data type ID 0x0002
     ECHOINTENSITY_PROFILE, //represents data type ID 0x0003
-    PERCENTGOOD_PROFILE  , //represents data type ID 0x0004
-    STATUS_PROFILE       , //represents data type ID 0x0005
-    BOTTOMTRACK_DATA     , //represents data type ID 0x0006
-    MICROCAT_DATA          //represents data type ID 0x0008
+    PERCENTGOOD_PROFILE, //represents data type ID 0x0004
+    STATUS_PROFILE, //represents data type ID 0x0005
+    BOTTOMTRACK_DATA, //represents data type ID 0x0006
+    MICROCAT_DATA //represents data type ID 0x0008
 }
--- src/main/java/edu/hawaii/soest/kilonalu/adcp/EnsembleMicroCAT.java
+++ src/main/java/edu/hawaii/soest/kilonalu/adcp/EnsembleMicroCAT.java
@@ -1,17 +1,12 @@
-/**
+/*
  *  Copyright: 2007 Regents of the University of Hawaii and the
  *             School of Ocean and Earth Science and Technology
- *    Purpose: A class that represents the ensemble MicroCAT data 
- *             component of data produced by an RDI 1200kHz Workhorse Acoustic 
- *             Doppler Current Profiler in the default PD0 format as described 
- *             in RDI's "Workhorse Commands and Output Data Format" manual, 
+ *    Purpose: A class that represents the ensemble MicroCAT data
+ *             component of data produced by an RDI 1200kHz Workhorse Acoustic
+ *             Doppler Current Profiler in the default PD0 format as described
+ *             in RDI's "Workhorse Commands and Output Data Format" manual,
  *             P/N 957-6156-00 (March 2005)
  *    Authors: Christopher Jones
- *
- * $HeadURL$
- * $LastChangedDate$
- * $LastChangedBy$
- * $LastChangedRevision$
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,30 +21,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */ 
+ */
 package edu.hawaii.soest.kilonalu.adcp;
 
-import edu.hawaii.soest.kilonalu.adcp.Ensemble;
-
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+
 /**
- *  A class that represents the MicroCAT data produced by
- *  an RDI 1200kHz Workhorse Acoustic Doppler Current Profiler in the
- *  default PD0 format. 
+ * A class that represents the MicroCAT data produced by
+ * an RDI 1200kHz Workhorse Acoustic Doppler Current Profiler in the
+ * default PD0 format.
  */
 public final class EnsembleMicroCAT {
 
-  /**
-   *  Constructor.  This method populates the MicroCAT fields from 
-   *  the given ByteBuffer of data passed in as an argument, based on metadata 
-   *  found in the EnsembleHeader.
-   *
-   * @param ensembleBuffer the ByteBuffer that contains the binary ensemble data
-   * @param ensembleHeader the ensembleHeader that contains the ensemble header data
-   */
-  public EnsembleMicroCAT( ByteBuffer ensembleBuffer, 
-                           Ensemble ensemble ) {
-    
-  }
+    /**
+     * Constructor.  This method populates the MicroCAT fields from
+     * the given ByteBuffer of data passed in as an argument, based on metadata
+     * found in the EnsembleHeader.
+     *
+     * @param ensembleBuffer the ByteBuffer that contains the binary ensemble data
+     * @param ensemble the ensemble that contains the ensemble header data
+     */
+    public EnsembleMicroCAT(ByteBuffer ensembleBuffer,
+                            Ensemble ensemble) {
+
+    }
 }
