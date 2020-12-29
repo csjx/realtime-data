25f971c4202e8b8ab57b4c4e1e0d4b009d772bfe
Christopher Jones <csj@christopherjones.co>
2020-12-29 13:55:58 -0700

    Add a ReaderTask class used to read in data files in parallel: Called by the ExecutorService, the call() method reads in the given data samples file, and returns a ReadResult object. The ReadResult includes the path to the file and the resulting table that was produced.
    refs https://github.com/csjx/realtime-data/issues/20

---
+++ src/main/java/edu/hawaii/soest/pacioos/text/concurrent/ReaderTask.java
@@ -0,0 +1,198 @@
+/*
+ *  Copyright: 2020 Regents of the University of Hawaii and the
+ *             School of Ocean and Earth Science and Technology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.hawaii.soest.pacioos.text.concurrent;
+
+import edu.hawaii.soest.pacioos.text.configure.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import tech.tablesaw.api.ColumnType;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.columns.datetimes.DateTimeColumnType;
+import tech.tablesaw.io.csv.CsvReadOptions;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+/**
+ * A task that produces a table from a single data file,
+ * producing a Table result with the table and the path to the file
+ */
+public class ReaderTask implements Callable<ReadResult> {
+
+    /* Set up a logger */
+    private final Log log = LogFactory.getLog(ReaderTask.class);
+
+    /* The path to the file that the table with be created from*/
+    private final Path path;
+
+    /* The instrument configuration used to build the table */
+    private final Configuration config;
+
+    /* The table created from the path */
+    private ReadResult readResult;
+
+    public ReaderTask(Path filePath, Configuration config) {
+        this.path = filePath;
+        this.config = config;
+    }
+
+    /**
+     * Generates a table, or throws an exception if unable to do so.
+     * @return table the generated table
+     * @throws Exception if unable to generate a table
+     */
+    @Override
+    public ReadResult call() throws Exception {
+
+
+        ReadResult readResult = new ReadResult(getPath());
+
+        try {
+            if (log.isDebugEnabled()) {
+                log.debug("Generating table for " + this.path.toString());
+            }
+            InputStream samples = new BufferedInputStream(
+                new FileInputStream(path.toString()));
+
+            // Make all dates a single column
+            String datePattern =
+                "([0-9][0-9]) " +                // day dd
+                "([A-Za-z][A-Za-z][A-Za-z]) " +  // mon MMM
+                "([0-9][0-9][0-9][0-9]), " +     // year yyyy, note comma
+                "([0-9][0-9]):" +                // hour HH
+                "([0-9][0-9]):" +                // min mm
+                "([0-9][0-9])";                  // sec ss
+            String dateReplacement = "$1 $2 $3 $4:$5:$6"; // note no comma
+
+
+            // Coerce the samples to into the correct table format
+            String samplesString = new String(samples.readAllBytes(), StandardCharsets.UTF_8);
+            samplesString = samplesString
+                .replaceAll("\r", "")
+                .replaceAll(",,", ",")
+                .replaceAll(datePattern, dateReplacement);
+
+            if ( samplesString.contains("#")) {
+                samplesString = samplesString
+                    .replaceAll(", *", ",")
+                    .replaceAll("\n", "")
+                    .replaceAll("# ", "\n")
+                    .replaceFirst("\n", "");
+            }
+            log.debug(samplesString);
+            samples.close();
+
+            // Create a new InputStream for to load the data into tablesaw
+            samples = new BufferedInputStream(
+                new ByteArrayInputStream(samplesString.getBytes(StandardCharsets.UTF_8)));
+
+            // Get or build the single datetime format
+            String datetimeFormat = config.getDateTimeFormat(0);
+            if (datetimeFormat == null) {
+                List<String> dateFormats = config.listDateFormats(0);
+                datetimeFormat = "";
+                for (int i = 0; i < dateFormats.size(); i++) {
+                    if (i == dateFormats.size() - 1) {
+                        datetimeFormat = datetimeFormat.concat(dateFormats.get(i));
+                    } else {
+                        datetimeFormat = datetimeFormat.concat(dateFormats.get(i) + " ");
+                    }
+                }
+            }
+
+            // Get the STRING column types, remove any DATE and TIME column types,
+            // and append a single DATETIME column type
+            ColumnType[] colTypes = config.getColumnTypes(0);
+            List<ColumnType> columnTypes =
+            Arrays.stream(colTypes).filter(columnType ->
+                columnType.toString().toLowerCase().equals("string")
+            ).collect(Collectors.toList());
+            columnTypes.add(DateTimeColumnType.instance());
+            colTypes = new ColumnType[columnTypes.size()];
+            columnTypes.toArray(colTypes);
+
+            CsvReadOptions.Builder builder =
+                CsvReadOptions.builder(samples)
+                    .commentPrefix("|".charAt(0))
+                    .lineEnding("\n")
+                    .header(false)
+                    .missingValueIndicator(config.getMissingValueCode(0))
+                    .sample(false)
+                    .separator(config.getFieldDelimiter(0).charAt(0))
+                    .tableName(getPath().toString())
+                    .columnTypes(colTypes);
+            builder.dateTimeFormat(DateTimeFormatter.ofPattern(datetimeFormat));
+
+            CsvReadOptions options = builder.build();
+            Table table = Table.read().usingOptions(options);
+            readResult.setTable(table);
+
+            samples.close();
+
+            if (log.isDebugEnabled()) {
+                log.debug("Generated table for " + this.path.toString());
+            }
+
+        } catch (Exception e) {
+
+            String msg = "For " + config.getIdentifier() + ", path " + path.toString() +
+                ". Couldn't generate the table correctly. The message was: " +
+                e.getMessage();
+            if (log.isDebugEnabled()) {
+                log.debug(msg);
+                e.printStackTrace();
+            }
+            throw e;
+        }
+        return readResult;
+    }
+
+    /**
+     * Get the path to the data file being processed
+      * @return path the data file path
+     */
+    public Path getPath() {
+        return path;
+    }
+
+    /**
+     * Get the instrument configuration for this task
+     * @return the instrument configuration
+     */
+    public Configuration getConfig() {
+        return config;
+    }
+
+    /**
+     * Get the ReadResult associated with the task
+     * @return readResult the table result
+     */
+    public ReadResult getReadResult() {
+        return readResult;
+    }
+}
