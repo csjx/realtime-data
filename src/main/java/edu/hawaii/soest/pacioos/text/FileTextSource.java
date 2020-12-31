cc87fa13857dd96d6fca63a3afd70e92f78582a9
Christopher Jones <csj@christopherjones.co>
2020-12-31 14:14:00 -0700

    Use instrument id and channel name in log statements.
    refs https://github.com/csjx/realtime-data/issues/20

--- src/main/java/edu/hawaii/soest/pacioos/text/FileTextSource.java
+++ src/main/java/edu/hawaii/soest/pacioos/text/FileTextSource.java
@@ -65,117 +65,123 @@ public class FileTextSource extends SimpleTextSource {
      * @see edu.hawaii.soest.pacioos.text.SimpleTextSource#execute()
      */
         protected boolean execute() {
-            log.debug("FileTextSource.execute() called.");
-            boolean failed = false; // indicates overall success of execute()
-            
+            log.debug("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                "FileTextSource.execute() called.");
+
             // do not execute the stream if there is no connection
             if ( !isConnected() ) return false;
-            
+
             try {
-                
+
                 // open the data file for monitoring
                 FileReader reader = new FileReader(new File(getDataFilePath()));
                 this.fileReader = new BufferedReader(reader);
-                
-                // add channels of data that will be pushed to the server.    
-                // Each sample will be sent to the Data Turbine as an rbnb frame.    
+
+                // add channels of data that will be pushed to the server.
+                // Each sample will be sent to the Data Turbine as an rbnb frame.
                 int channelIndex = 0;
-                
+
                 long lastSampleTimeAsSecondsSinceEpoch = getLastSampleTime();
-                
+
                 // poll the data file for new lines of data and insert them into the RBNB
                 while (true) {
                     String line = fileReader.readLine();
-                    
+
                     if (line == null ) {
                         Thread.sleep(this.pollInterval);
-                        
+
                     } else {
 
                         // if the line matches the data pattern, insert it into the DataTurbine
                         if ( this.validateSample(line) ) {
-                            log.debug("This line matches the data line pattern: " + line);
-                            
+                            log.debug("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                "This line matches the data line pattern: " + line);
+
                             Instant sampleInstant = getSampleInstant(line);
 
-                            log.debug("Sample instant is: " + sampleInstant.toString());
-                            
+                            log.debug("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                "Sample instant is: " + sampleInstant.toString());
+
                             // convert the sample instant to seconds since the epoch
                             long sampleTimeAsSecondsSinceEpoch = sampleInstant.getEpochSecond();
-                            
-                            // only insert samples newer than the last sample seen at startup 
+
+                            // only insert samples newer than the last sample seen at startup
                             // and that are not in the future (> 1 hour since the CTD clock
                             // may have drifted)
                             ZonedDateTime now = ZonedDateTime.now(this.tz.toZoneId());
 
                             if ( lastSampleTimeAsSecondsSinceEpoch < sampleTimeAsSecondsSinceEpoch &&
                                  sampleTimeAsSecondsSinceEpoch < now.plusHours(1L).toEpochSecond() ) {
-                                
+
                                 try {
                                     // insert into the DataTurbine
                                     this.sendSample(line);
-                                
+
                                 } catch ( SAPIException sapie ) {
                                     // reconnect if an exception is thrown on Flush()
-                                    log.error("Error while flushing the source: " + sapie.getMessage());
+                                    log.error("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                        "Error while flushing the source: " + sapie.getMessage());
                                     sapie.printStackTrace();
                                 }
-                                
+
                                 // reset the last sample time to the sample just inserted
                                 lastSampleTimeAsSecondsSinceEpoch = sampleTimeAsSecondsSinceEpoch;
-                                log.debug("Last sample time is now: " +
+                                log.debug("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                    "Last sample time is now: " +
                                     Instant.ofEpochSecond(lastSampleTimeAsSecondsSinceEpoch));
-                                log.info(getRBNBClientName() + " Sample sent to the DataTurbine: " +
+                                log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                    " Sample sent to the DataTurbine: " +
                                     line.trim());
-                                
+
                             } else {
-                                log.info("The current line is earlier than the last entry " +
+                                log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                    "The current line is earlier than the last entry " +
                                     "in the Data Turbine or is a date in the future. " +
                                     "Skipping it. The line was: " + line);
-                                
+
                             }
-                            
+
                         } else {
-                            log.info("The current line doesn't match an expected " +
+                            log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                                "The current line doesn't match an expected " +
                                 "data line pattern. Skipping it.    The line was: " + line);
-                                                    
+
                         }
                     }    // end if()
                 } // end while
-            
+
             } catch ( ParseException pe ) {
-                log.info("There was a problem parsing the sample date string. The message was: " + pe.getMessage());
-                failed = true;
+                log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                    "There was a problem parsing the sample date string. The message was: " + pe.getMessage());
                 return false;
-                
+
             } catch ( SAPIException sapie ) {
-                log.info("There was a problem communicating with the DataTurbine. The message was: " + sapie.getMessage());
-                failed = true;
+                log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                    "There was a problem communicating with the DataTurbine. The message was: " + sapie.getMessage());
                 return false;
-                
+
             } catch ( InterruptedException ie ) {
-                log.info("There was a problem while polling the data file. The message was: " + ie.getMessage());
-                failed = true;
+                log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                    "There was a problem while polling the data file. The message was: " + ie.getMessage());
                 return false;
-                
+
             } catch ( IOException ioe ) {
-                log.info("There was a problem opening the data file. The message was: " + ioe.getMessage());
-                failed = true;
+                log.info("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                    "There was a problem opening the data file. The message was: " + ioe.getMessage());
                 return false;
-                
+
             } finally {
                 try {
                     if ( this.fileReader != null ) {
                         this.fileReader.close();
                     }
-                    
-                } catch (IOException ioe2) {
-                    log.error("There was a problem closing the data file. The message was: " + ioe2.getMessage());
-                    
-                }
 
+                } catch (IOException ioe2) {
+                    log.error("[" + getIdentifier() + "/" + getChannelName() + " ] " +
+                        "There was a problem closing the data file. The message was: " + ioe2.getMessage());
+
+                }
             }
-        
     }
   
     /* (non-Javadoc)
