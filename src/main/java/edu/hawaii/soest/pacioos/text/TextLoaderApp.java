add89ff96f11f99c7392a2cea6e2861328dbb5d7
Christopher Jones <csj@christopherjones.co>
2021-12-25 23:44:21 -0700

    Stub out a TextLoaderApp that will push samples to the DT:
    Use the TextRebuilder to read a year's worth of data, and recreate the text samples to be pushed into a ChannelMap and sent to the DataTurbine.  This will be called by the manage-instruments script.
    
    https://github.com/csjx/realtime-data/issues/40

---
+++ src/main/java/edu/hawaii/soest/pacioos/text/TextLoaderApp.java
@@ -0,0 +1,293 @@
+/*
+ *  Copyright: 2021 Regents of the University of Hawaii and the
+ *             School of Ocean and Earth Science and Technology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.hawaii.soest.pacioos.text;
+
+import com.rbnb.sapi.ChannelMap;
+import com.rbnb.sapi.SAPIException;
+import com.rbnb.sapi.Source;
+import edu.hawaii.soest.helpers.ConsoleColors;
+import edu.hawaii.soest.pacioos.text.configure.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import tech.tablesaw.api.DateTimeColumn;
+import tech.tablesaw.api.InstantColumn;
+import tech.tablesaw.api.Row;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+/**
+ * An application to load text-based data into the DataTurbine
+ *
+ *
+ */
+public class TextLoaderApp {
+
+    /* Set up a log */
+    private static final Log log = LogFactory.getLog(TextLoaderApp.class);
+
+    /**
+     * The main entrypoint
+     * @param args the arguments array providing the path to the instrument config file
+     *             and the path to the raw data directory
+     */
+    public static void main(String[] args) {
+
+        /* The path to the configuration file */
+        String xmlConfiguration = null;
+        /* The filesystem path to process */
+        String dataDirectoryPath = null;
+
+        if (args.length != 2) {
+            log.error("Please provide the path to the instrument's XML configuration file.");
+            System.exit(1);
+        } else {
+            xmlConfiguration = args[0];
+            dataDirectoryPath = args[1];
+
+        }
+
+        // Create a TextRebuilder instance
+        TextRebuilder.Builder builder =
+            TextRebuilder.builder(xmlConfiguration).dataDirectory(dataDirectoryPath);
+        TextRebuilder rebuilder = builder.build();
+
+        // Parse the configuration file
+        Configuration config = rebuilder.getConfiguration();
+
+        Table mergedTable = rebuilder.getMergedTable();
+
+        if ( mergedTable != null ) {
+
+            // Is the datetime column first or last?
+            int firstDateField = Integer.parseInt(config.getDateFields(0).get(0));
+            // Sort the table using the datetime field's corresponding index
+            int sortColumnIndex = firstDateField <= 1 ? 0 : mergedTable.columnCount() - 1;
+
+            // Deduplicate and sort rows of the merged table
+            Table sortedTable = rebuilder.deduplicateAndSortTable(mergedTable, sortColumnIndex);
+
+            Table filteredTable = getFilteredTable(config, firstDateField, sortedTable);
+
+            log.info(
+                ConsoleColors.BLUE +
+                    "Loading filtered table into the Data Turbine:\n" +
+                    filteredTable +
+                    ConsoleColors.RESET
+            );
+            log.info("Filtered table count: " + filteredTable.rowCount());
+
+            try {
+                clearInstrumentData(config);
+
+                sendSamples(config, filteredTable);
+
+            } catch (SAPIException e) {
+                e.printStackTrace();
+            }
+        }
+        // Clean up
+        rebuilder.shutdownExecutorService();
+    }
+
+    private static Table getFilteredTable(Configuration config, int firstDateField, Table sortedTable) {
+        DateTimeColumn dateTimeColumn;
+        InstantColumn instantColumn;
+        // Add a table Instant column for filtering by day and hour
+        if ( firstDateField > 1 ) {
+            dateTimeColumn = (DateTimeColumn) sortedTable.column(sortedTable.columns().size() - 1);
+            instantColumn = dateTimeColumn.asInstantColumn(ZoneId.of(config.getTimeZoneID(0)));
+            instantColumn.setName("datetimesInUTC");
+            sortedTable.addColumns(instantColumn);
+            // Drop the original date time column since it's no longer needed
+            sortedTable.removeColumns(sortedTable.columnCount() - 2);
+        } else {
+            dateTimeColumn = (DateTimeColumn) sortedTable.column(0);
+            instantColumn = dateTimeColumn.asInstantColumn(ZoneId.of(config.getTimeZoneID(0)));
+            instantColumn.setName("datetimesInUTC");
+            sortedTable.addColumns(instantColumn);
+            // Drop the original date time column since it's no longer needed
+            sortedTable.removeColumns(0);
+        }
+
+        Instant oneYearPriorDateInUTC = instantColumn.max().minus(365, ChronoUnit.DAYS);
+
+        // Filter the table for the last year's worth of data starting from the most recent sample
+        Table filteredTable = sortedTable.where(instantColumn.isAfter(oneYearPriorDateInUTC));
+        return filteredTable;
+    }
+
+
+    private static void sendSamples(Configuration config, Table filteredTable) throws SAPIException {
+        Source rbnbSource = new Source(config.getArchiveMemory(), "create", config.getArchiveSize());
+         rbnbSource.OpenRBNBConnection(
+             config.getServerName() + ":" + config.getServerPort(),
+             config.getIdentifier()
+         );
+        // Create a channel map with two channels, raw and processed
+
+        // Construct raw and processed datetime formatters based on the configuration info
+        double sampleTimeAsSecondsSinceEpoch;
+        List<String> dateFields = config.getDateFields(0);
+        List<String> dateFormats = config.getDateFormats(0);
+        int firstDateFieldPosition = Integer.parseInt(dateFields.get(0));
+        String dateTimeSeparator = firstDateFieldPosition > 1 && dateFields.size() > 1 ? "," : " ";
+        String timeZoneID = config.getTimeZoneID(0);
+        String rawFieldDelimiter = config.getFieldDelimiter(0);
+        // handle hex-encoded field delimiters
+        if ( rawFieldDelimiter.startsWith("0x") || rawFieldDelimiter.startsWith("\\x" ) ||
+            rawFieldDelimiter.startsWith("0X") || rawFieldDelimiter.startsWith("\\X" )) {
+
+            byte delimBytes = Byte.parseByte(rawFieldDelimiter.substring(2), 16);
+            byte[] delimAsByteArray = new byte[]{delimBytes};
+            rawFieldDelimiter = new String(
+                delimAsByteArray, 0,
+                delimAsByteArray.length,
+                StandardCharsets.UTF_8);
+        }
+        String processedFieldDelimiter = ",";
+        String dataPrefix = config.getDataPrefix(0);
+        String recordDelimiter = config.getRecordDelimiter(0);
+
+        DateTimeFormatter rawFormatter;
+        DateTimeFormatter processedFormatter = DateTimeFormatter.ISO_INSTANT;
+        StringBuilder dateFormatsStr;
+        if ( dateFields.size() == 1 ) {
+            rawFormatter =
+                DateTimeFormatter.ofPattern(dateFormats.get(0)).withZone(ZoneId.of(timeZoneID));
+        } else {
+            dateFormatsStr = new StringBuilder();
+            int index = 0;
+            for (String dateFormat : dateFormats) {
+                    dateFormatsStr.append(dateFormat);
+                    if (index < dateFormats.size() - 1) {
+                        dateFormatsStr.append(dateTimeSeparator);
+                    }
+                    index++;
+            }
+            rawFormatter = DateTimeFormatter.ofPattern(dateFormatsStr.toString());
+        }
+
+        ChannelMap rbnbChannelMap = new ChannelMap();
+        int channelIndex = rbnbChannelMap.Add(config.getChannelName(0));
+        int convertedChannelIndex = rbnbChannelMap.Add("PacIOOS2020Format");
+        rbnbChannelMap.PutMime(channelIndex, "text/plain");
+        rbnbChannelMap.PutMime(convertedChannelIndex, "text/plain");
+
+        for (Row row : filteredTable) {
+            StringBuilder rawSample = new StringBuilder(); // Most performant inside loop per SO article
+            StringBuilder processedSample = new StringBuilder();
+            StringColumn[] stringColumns =  filteredTable.stringColumns();
+            if ( dataPrefix != null && ! dataPrefix.isEmpty() ) {
+                rawSample.append(dataPrefix);
+                rawSample.append(" ");
+            }
+
+            Instant sampleInstant = row.getInstant("datetimesInUTC");
+            sampleTimeAsSecondsSinceEpoch = sampleInstant.getEpochSecond();
+            rbnbChannelMap.PutTime(sampleTimeAsSecondsSinceEpoch, 0d);
+            // Build the raw date/time columns or datetime column, determine column position
+            if ( firstDateFieldPosition == 1 ) {
+                rawSample.append(rawFormatter.format(sampleInstant.atZone(ZoneOffset.ofHours(0))));
+                rawSample.append(rawFieldDelimiter);
+                rawSample.append(" ");
+                processedSample.append(processedFormatter.format(sampleInstant));
+                processedSample.append(processedFieldDelimiter);
+
+                // Append each raw data column value after the datetime column
+                int index = 0;
+                for (StringColumn column : stringColumns) {
+                    rawSample.append(column.get(0));
+                    processedSample.append(column.get(0));
+
+                    if ( index < stringColumns.length - 1) {
+                        rawSample.append(rawFieldDelimiter);
+                        rawSample.append(" ");
+                        processedSample.append(processedFieldDelimiter);
+                    }
+                    index++;
+                }
+                rawSample.append(recordDelimiter);
+                processedSample.append("\n");
+
+                log.info(rawSample.toString().trim());
+                log.info(processedSample.toString());
+            } else {
+                // Append each raw data column before the date time for raw data only
+                processedSample.append(processedFormatter.format(sampleInstant));
+                processedSample.append(processedFieldDelimiter);
+
+                int index = 0;
+                for (StringColumn column : stringColumns) {
+                    rawSample.append(column.get(0));
+                    processedSample.append(column.get(0));
+                    rawSample.append(rawFieldDelimiter);
+                    rawSample.append(" ");
+
+                    if ( index < stringColumns.length - 1) {
+                        processedSample.append(processedFieldDelimiter);
+                    }
+                    index++;
+                }
+                rawSample.append(rawFormatter.format(sampleInstant));
+                rawSample.append(recordDelimiter);
+                processedSample.append("\n");
+
+                log.info(rawSample.toString().trim());
+                log.info(processedSample.toString());
+
+            }
+            // Build raw sample based on position, add raw data plus date, use formatter
+
+            // Build converted sample, use formatter
+
+            // Add to each channel
+
+        }
+
+        // Connect
+
+        // Flush channelmap
+
+        // Disconnect
+    }
+
+    /**
+     * Remove Data Turbine channel data for the given instrument by resetting the ring buffer
+     * @param config the instrument XML configuration
+     * @throws SAPIException a Data Turbine exception
+     */
+    private static void clearInstrumentData(Configuration config) throws SAPIException {
+        Source rbnbSource = new Source(config.getArchiveMemory(), "create", config.getArchiveSize());
+        rbnbSource.OpenRBNBConnection(
+            config.getServerName() + ":" + config.getServerPort(),
+            config.getIdentifier()
+        );
+        rbnbSource.Detach();
+        rbnbSource.CloseRBNBConnection();
+    }
+}
